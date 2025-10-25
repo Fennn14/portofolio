e917935efd1adca782675df1a6b385a8c81ffdf2
fennn <132808858+Fennn14@users.noreply.github.com>
2025-10-25 19:02:08 +0700

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,204 @@
+import HeroImage from "/assets/hero-img.webp";
+
+const Image = {
+  HeroImage,
+};
+
+export default Image;
+
+import Tools1 from "/assets/tools/vscode.png";
+import Tools2 from "/assets/tools/reactjs.png";
+import Tools5 from "/assets/tools/bootstrap.png";
+import Tools6 from "/assets/tools/js.png";
+import Tools7 from "/assets/tools/nodejs.png";
+import Tools8 from "/assets/tools/github.png";
+import Tools10 from "/assets/tools/canva.png";
+import Tools11 from "/assets/tools/figma.png";
+import Tools13 from "/assets/tools/firebase.png";
+import Tools14 from "/assets/tools/html.png";
+import Tools15 from "/assets/tools/css.png";
+import Tools17 from "/assets/tools/php.png";
+import Tools18 from "/assets/tools/vite.png";
+import Tools19 from "/assets/tools/mysql.png";
+
+
+export const listTools = [
+  {
+    id: 1,
+    gambar: Tools1,
+    nama: "Visual Studio Code",
+    ket: "Code Editor",
+    dad: "100",
+  },
+  {
+    id: 2,
+    gambar: Tools2,
+    nama: "React JS",
+    ket: "Framework",
+    dad: "200",
+  },
+ 
+  {
+    id: 5,
+    gambar: Tools5,
+    nama: "Bootstrap",
+    ket: "Framework",
+    dad: "500",
+  },
+  {
+    id: 6,
+    gambar: Tools6,
+    nama: "Javascript",
+    ket: "Language",
+    dad: "600",
+  },
+  {
+    id: 7,
+    gambar: Tools7,
+    nama: "Node JS",
+    ket: "Javascript Runtime",
+    dad: "700",
+  },
+  {
+    id: 8,
+    gambar: Tools8,
+    nama: "Github",
+    ket: "Repository",
+    dad: "800",
+  },
+ 
+  {
+    id: 10,
+    gambar: Tools10,
+    nama: "Canva",
+    ket: "Design App",
+    dad: "1000",
+  },
+  {
+    id: 11,
+    gambar: Tools11,
+    nama: "Figma",
+    ket: "Design App",
+    dad: "1100",
+  },
+
+  {
+    id: 13,
+    gambar: Tools13,
+    nama: "Firebase",
+    ket: "Framework",
+    dad: "1300",
+  },
+  {
+    id: 14,
+    gambar: Tools14,
+    nama: "HTML",
+    ket: "Language",
+    dad: "1400",
+  },
+  {
+    id: 15,
+    gambar: Tools15,
+    nama: "CSS",
+    ket: "Language",
+    dad: "1500",
+  },
+
+  {
+    id: 17,
+    gambar: Tools17,
+    nama: "PHP",
+    ket: "Language",
+    dad: "1700",
+  },
+  {
+    id: 18,
+    gambar: Tools18,
+    nama: "Vite",
+    ket: "Framework",
+    dad: "1800",
+  },
+  {
+    id: 19,
+    gambar: Tools19,
+    nama: "MySql",
+    ket: "Framework",
+    dad: "1900",
+  },
+];
+
+import Proyek1 from "/assets/proyek/proyek1.jpg";
+import Proyek2 from "/assets/proyek/proyek2.jpg";
+import Proyek3 from "/assets/proyek/proyek3.jpg";
+import Proyek4 from "/assets/proyek/proyek4.jpg";
+import Proyek5 from "/assets/proyek/proyek5.jpg";
+import Proyek6 from "/assets/proyek/proyek6.jpg";
+
+export const listProyek = [
+  {
+    id: 1,
+    image: Proyek1,
+    title: "Advanced Encryption Standard (AES) Method128-Bit Web-Based",
+    subtitle: "Application for converting confidential documents into encrypted and 	decrypted ones...",
+    fullDescription:"Application for converting confidential documents into encrypted and 	decrypted ones.",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+   
+    dad: "100",
+  },
+  {
+    id: 2,
+    image: Proyek2,
+    title: "Optimization of the Asana Platform in Publicity Teamwork Management",
+    subtitle: "Analyzing the use of the Asana application to improve employee time 	management....",
+    fullDescription:"Analyzing the use of the Asana application to improve employee time 	management",
+    borderColor: "#10B981",
+    gradient: "linear-gradient(180deg, #10B981, #000)",
+   
+    dad: "200",
+  },
+  {
+    id: 3,
+    image: Proyek3,
+    title: "Design of a Village Information System in Namo Buaya Village, Sultan Daulat District, Subulussalam City Using OpenSID",
+    subtitle: "Creating an OpenSID-based village website....",
+    fullDescription:"Creating an OpenSID-based village website.",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+   
+    dad: "300",
+  },
+  {
+    id: 4,
+    image: Proyek4,
+    title: "Personal Web Portfolio",
+    subtitle: "An interactive web portfolio showcasing my professional journey...",
+    fullDescription:"An interactive web portfolio showcasing my professional journey, skills, and projects. Designed with a clean yet modern aesthetic, the portfolio highlights my technical expertise in web development, software engineering, and IoT. The site also serves as a central hub for potential employers and collaborators to explore my works, reflecting both my creativity and technical precision.",
+    borderColor: "#10B981",
+    gradient: "linear-gradient(180deg, #10B981, #000)",
+   
+    dad: "400",
+  },
+    {
+    id: 5,
+    image: Proyek5,
+    title: "Design and Development of a Web-Based Boarding House Management Information System",
+    subtitle: "Web application development for boarding house owners in the Meurebo area",
+    fullDescription:"Developing a web application for archives management for BPS 	employees.",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+
+    dad: "500",
+  },
+  {
+    id: 6,
+    image: Proyek6,
+    title: "Coffee Shop Website",
+    subtitle: "A fully functional website designed for a coffee shop, enabling...",
+    fullDescription:"A fully functional website designed for a coffee shop, enabling customers to explore the menu, learn about the shop’s story, and place orders online. The project focused on creating an attractive and modern interface, integrating responsive design for mobile and desktop, and ensuring smooth navigation for users. This website not only improved customer experience but also supported the coffee shop’s digital presence and business growth.",
+    borderColor: "#10B981",
+    gradient: "linear-gradient(180deg, #10B981, #000)",
+   
+    dad: "600",
+  },
+];
